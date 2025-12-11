c8a441a2ad97255d894f2aed3dd9b1fceacb09b7
Nathan H <22816739+nathub@users.noreply.github.com>
2025-12-10 21:45:04 -0400

    Day1 Part 2 attempted solution - wrong

--- aoc_day1p2.rb
+++ aoc_day1p2.rb
@@ -1,31 +1,57 @@
 def parse(rotationString)
-	rotationString = rotationString.gsub(/\s+/, "")
-	directionString = rotationString[0]
-	valueString = rotationString[1, rotationString.length]
-
+	rotationString = rotationString.gsub(/\s+/, "")			#Remove all whitespace characters
+	directionString = rotationString[0]						#Direction is always first character
+	valueString = rotationString[1, rotationString.length]	#Rotation amount is the number in the string.
+	#Parse remaining of the string as the rotation amount. If it doesn't parse, set to nil.
 	value = valueString.to_i != 0 || valueString.match(/^0+$/) ? valueString.to_i : nil
-	
+
+	#Parse L or l as Left (negative rotation), and R or r as Right (positive). 0 is invalid.
 	direction = directionString =~ /[L|l]/ ? -1 : 0
 	direction = directionString =~ /[R|r]/ ? 1 : direction
 
-	if (value.nil? || direction == 0) then raise "Rotation Parse Error! #{rotationString}" end
 	
+	if (value.nil? || direction == 0) then raise "Rotation Parse Error! #{rotationString}" end
+
+	#Results
+	puts "Rotation String: #{rotationString}"	if @DEBUG
+	puts "Rotation Parsed: #{direction} #{value}"	if @DEBUG
 	return value * direction
 end
 
-@DEBUG == true
+def zerosPassed(oldPosition, newPosition, rotation)
+	oldHundreds = oldPosition / 100
+	newHundreds = newPosition / 100
+	diff = oldHundreds - newHundreds
+	special = rotation %100
+
+	puts "Old, New, and Diffrence: #{oldPosition} #{oldHundreds} #{newPosition} #{newHundreds} #{diff}" if @DEBUG
+	puts "Special: #{special}" if @DEBUG
+
+	return special == 0 ? diff.abs - 1 : diff.abs
+end
+
+@DEBUG = true
 if __FILE__ == $0
 puts "Start."
 	if (!ARGV[0].nil?)
 		puts "ARGV not nil #{ARGV[0]}"
 		position = 50
+		count = 0 #number of zeros passed and landed on
 		File.open(ARGV[0]).each do |rotationString|
 			rotation = parse(rotationString)
 			puts "Rotation: #{rotation}" if @DEBUG
 			puts "Position #{position}" if @DEBUG
+			oldPosition = position
 			position = position + rotation
 			puts "New Position: #{position}" if @DEBUG
+			passed = zerosPassed(oldPosition, position, rotation)
+			puts "Passed Zeros: #{passed}" if @DEBUG
+			onZero = position%100 == 0 ? 1 : 0
+			count += (onZero + passed)
 			puts "Mod Position: #{position%100}" if @DEBUG
+			position = position % 100
+
+			puts "Count: #{count}"
 		end
 	end
 
