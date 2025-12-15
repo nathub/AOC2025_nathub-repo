4a8ea3fe8be3708cda7d89b959dcb4d150cc887d
Nathan H <22816739+nathub@users.noreply.github.com>
2025-12-15 00:24:16 -0400

    WIP rework , using pumping lemma method instead.

--- aoc_day1p2.rb
+++ aoc_day1p2.rb
@@ -18,40 +18,59 @@ def parse(rotationString)
 	return value * direction
 end
 
-def zerosPassed(oldPosition, newPosition, rotation)
-	oldHundreds = oldPosition / 100
-	newHundreds = newPosition / 100
-	diff = oldHundreds - newHundreds
-	special = rotation %100
-
-	puts "Old, New, and Diffrence: #{oldPosition} #{oldHundreds} #{newPosition} #{newHundreds} #{diff}" if @DEBUG
-	puts "Special: #{special}" if @DEBUG
-
-	return special == 0 ? diff.abs - 1 : diff.abs
+def executeRotation(position, rotation)
+	puts "executeRotation"
+	puts "Current Position: #{position}, Rotation: #{rotation}"
+	passedZeroes = 0
+	counter = 0
+	while rotation != 0 do
+		puts "While Do Position #{position}, rotation #{rotation}, passedZeroes #{passedZeroes}."
+		if(position + rotation < 0) 
+			puts "IS LESS THAN ZERO - P+R #{position + rotation}, P #{position}, R #{rotation}"
+			rotation += position != 0 ? position : position
+			passedZeroes += 1
+			position = 0
+			puts "LESS THAN RESULT: Rotation: #{rotation}, passedZeroes #{passedZeroes}, position: #{position}."
+		elsif(position + rotation > 100)
+			puts "IS GREATER THAN HUNDRED - P+R #{position + rotation}, P #{position}, R #{rotation}"
+			rotation -= (100 - position)
+			passedZeroes += 1
+			position = 0
+		else
+			puts "IS WITHIN BOUNDS - P+R #{position + rotation}, P #{position}, R #{rotation}"
+			position = position + rotation
+			rotation = rotation - rotation
+		end
+		counter+= 1
+		if counter > 10 then raise "Loop has gone 10 times, raising error!" end
+	end
+	puts "passedZeroes: #{passedZeroes}"
+	puts "Position Result: #{position}"
+	position = position % 100 #In the event it is 100, make it 0
+	zeroes = position == 0 ? passedZeroes + 1 : passedZeroes
+	return position%100 , zeroes
 end
 
-@DEBUG = true
+@DEBUG = false
 if __FILE__ == $0
 puts "Start."
 	if (!ARGV[0].nil?)
 		puts "ARGV not nil #{ARGV[0]}"
 		position = 50
 		count = 0 #number of zeros passed and landed on
+		passCount = 0
 		File.open(ARGV[0]).each do |rotationString|
 			rotation = parse(rotationString)
 			puts "Rotation: #{rotation}" if @DEBUG
 			puts "Position #{position}" if @DEBUG
-			oldPosition = position
-			position = position + rotation
-			puts "New Position: #{position}" if @DEBUG
-			passed = zerosPassed(oldPosition, position, rotation)
-			puts "Passed Zeros: #{passed}" if @DEBUG
-			onZero = position%100 == 0 ? 1 : 0
-			count += (onZero + passed)
-			puts "Mod Position: #{position%100}" if @DEBUG
-			position = position % 100
 
-			puts "Count: #{count}"
+			oldPosition = position
+			position, thisCount = executeRotation(position, rotation)
+			puts "New Position: #{position}, added this many zeroes: #{thisCount}" #if @DEBUG
+
+			count += thisCount
+			
+			puts "Count: #{count} This Rotation\'s Count: #{thisCount}, Rotation String: #{rotationString}"
 		end
 	end
 
